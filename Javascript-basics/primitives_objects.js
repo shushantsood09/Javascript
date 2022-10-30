15d0c8642d61d4eee6d0641ee193cd548bdd165c
shushantsood09 <shushantsood89012@gmail.com>
2022-10-30 12:48:21 +0530

    primitive data types

---
+++ Javascript-basics/primitives_objects.js
@@ -0,0 +1,12 @@
+// nn bb ss u  // Trick to learn data types.
+
+let a = null;
+let b = 200;
+let c = true;  //can also be false.
+let d = BigInt("999") + BigInt("1");
+let e = "Shushant";
+let f = Symbol("I am a nice symbol");
+let g = undefined;
+console.log(a, b, c, d, e, f, g );
+
+console.log(typeof d) //Console to get the type.
