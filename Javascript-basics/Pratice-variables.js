697ac408fd6308290959eb0f029d69d9eca6ce6a
shushantsood09 <shushantsood89012@gmail.com>
2022-11-03 13:58:16 +0530

    Practice set 1 solutions

---
+++ Javascript-basics/Pratice-variables.js
@@ -0,0 +1,34 @@
+// Chapter-1 - Q1
+let a = "Shushant";
+let b = 6;
+console.log(a + b);
+
+// Chapter-1 - Q2
+console.log(typeof(a+b));
+
+// Chapter-1 - Q3
+const obj = {
+    name: "Shushant",
+    class: 12
+}
+console.log(obj);
+
+// Chapter-1 - Q4
+obj['friend'] = "Sumit";
+console.log(obj);
+obj['friend'] = "lovish";
+console.log(obj);
+
+// The below one is wrong.
+// obj = 10;
+// console.log(obj);
+
+// Chapter-1 - Q5
+
+const dict = {
+    "appericiate" : "recagnize the worth of.",
+    "frredom": "Freeness",
+    "yakka": "Hard work",
+}
+
+console.log(dict.yakka);
