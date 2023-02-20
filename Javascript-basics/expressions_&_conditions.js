f6cb872de08d3175fbc0e2ab36dc73f5f9582ccf
shushantsood09 <shushantsood89012@gmail.com>
2023-02-20 18:22:30 +0530

    adding expressions and conditions

---
+++ Javascript-basics/expressions_&_conditions.js
@@ -0,0 +1,18 @@
+//Valid javascript code.
+88;
+"Vinod"; // string literal
+
+
+// Operators in JS.
+let a = 90;
+let b = 5;
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a * b = ", a*b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b)
+console.log("a % b = ", a%b)
+console.log("a++ = ", a++);
+console.log("a = ",a)
+console.log("a-- = ", a--)
+console.log("a = ",a)
