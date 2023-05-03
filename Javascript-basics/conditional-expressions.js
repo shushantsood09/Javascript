97aeeae294c207d5114ca1b9d1208ee00f087895
shushantsood09 <shushantsood89012@gmail.com>
2023-05-03 13:30:51 +0530

    conditional statements

---
+++ Javascript-basics/conditional-expressions.js
@@ -0,0 +1,10 @@
+// let a  = prompt("Hey this is Shushant!!");
+// a = Number.parseInt(a);
+
+let data = 10;
+if(data >= 10){
+    console.log("Data is grater");
+}
+else{
+    console.log("Data is smaller");
+}
