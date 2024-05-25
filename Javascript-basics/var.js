3d791909defa300eae47a36ef73a519fc172c055
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2024-05-25 12:27:05 +0530

    variables in js.

---
+++ Javascript-basics/var.js
@@ -0,0 +1,11 @@
+var myName = "abba"   // Var is going to be used throughout the whole program.
+
+myName = 8; 
+let ourName = "MyName" // Let is going to be use in the scope of where we declare that.
+
+const pi = 3.14 // Variable that value can never change.
+
+// Javascript is a case sensitive language whicch means the declarations will be different if having same name with differnt case.
+// For example :  
+// var myName;
+// var MyName;   Both the variables are different.
