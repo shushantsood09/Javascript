410e3674849dc2d23bde0abaf3b6075dd4279c91
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2024-12-06 23:37:46 +0530

    operators in JS

---
+++ Javascript-basics/operators.js
@@ -0,0 +1,43 @@
+console.log("Operators in JS");
+
+//Arithmetic Operators
+let a = 10;
+let b = 4;
+
+console.log("a + b = ", a + b);
+console.log("a - b = ", a - b);
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+console.log("a ** b = ", a ** b);
+console.log("a % b = ", a % b);
+console.log("++a = ", ++a);   //vala = 11
+console.log("a++ = ", a++);   //vala = 11
+console.log("--a = ", --a);   //vala = 11
+console.log("a-- = ", a--);   //vala = 11
+// console.log("a % b = ", a % b);
+
+
+// Assignment Operators
+let assignment = 1;
+assignment += 5;  // same as a+=5
+console.log(assignment);
+
+
+// Comparision operators
+let comp1 = 5;
+let comp2 = 6;
+console.log("comp1 == comp2 is", comp1 == comp2);
+console.log("comp1 != comp2 is", comp1 != comp2);
+console.log("comp1 == comp2 is", comp1 === comp2);
+console.log("comp1 == comp2 is", comp1 !== comp2);
+console.log("comp1 > comp2 is", comp1 > comp2);
+
+// :ogical Operators
+let x = 5;
+let y = 6;
+console.log(x < y && x==5);
+console.log(x > y || x==5);
+console.log(!true);
+
+
+
