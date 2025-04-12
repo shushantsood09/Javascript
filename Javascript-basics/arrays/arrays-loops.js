eaf54abfb78fd613536d110bb8afd428a05fd7a3
shushant sood <104362678+shushantsood09@users.noreply.github.com>
2025-04-12 18:09:59 +0530

    array loops basicxs.

---
+++ Javascript-basics/arrays/arrays-loops.js
@@ -0,0 +1,24 @@
+let num = [3, 54, 1, 2, 24];
+
+// Classic for Loop.
+// for(let i =0; i< num.length; i++){
+//     console.log(num[i]);
+// }
+
+// ForEach loop.
+num.forEach((element) => {
+    console.log(element * element);
+});
+
+// Array.from
+
+let data = "Shushant";
+let arr = Array.from(data);   //  This means prepare an array from this string.
+console.log(arr)
+
+// for...of loop  - It is a shortcut way to access the array elements.
+for(let i of num){
+    console.log(i)
+}
+
+
